e6eacdcbbb5c48ce552b40fe882a7929488e4476
bdeglane <bdeglane@users.noreply.github.com>
2019-01-10 21:02:56 +0100

    :white_check_mark: update example

--- src/component/input/input.component.test.js
+++ src/component/input/input.component.test.js
@@ -1,42 +1,62 @@
 import React from 'react';
+import ReactDOMServer from 'react-dom/server';
 import axe from 'axe-core';
-
+import { axe as jestAxe } from 'jest-axe';
 import { Input } from './input.component';
 import { mountToDoc } from '../../setupTests';
 
 
 describe('<Input />', () => {
+    describe('with axe', () => {
 
-    let input;
-    let component;
+        let input;
+        let component;
 
-    beforeEach(() => {
+        beforeEach(() => {
 
-        input = mountToDoc(<Input
-            type="text"
-            name="test"
-            id="test"
-            value="test"
-            onChange={(c) => c}
-        />);
+            input = mountToDoc(<Input
+                type="text"
+                name="test"
+                id="test"
+                value="test"
+                onChange={(c) => c}
+            />);
 
-        component = input.getDOMNode();
-    });
-
-
-    test('a11y', async () => {
-
-        const res = await axe.run(component, {
-            runOnly: {
-                values: ["wcag2a", "wcag2aa"]
-            },
-            resultTypes: ["violations"]
+            component = input.getDOMNode();
         });
 
-        if (res.violations.length > 0) {
-            console.log(res.violations);
-        }
 
-        expect(res.violations.length).toBe(0);
+        test('a11y', async () => {
+
+            const res = await axe.run(component, {
+                runOnly: {
+                    values: ["wcag2a", "wcag2aa"]
+                },
+                resultTypes: ["violations"]
+            });
+
+            if (res.violations.length > 0) {
+                console.log(res.violations);
+            }
+
+            expect(res.violations.length).toBe(0);
+        });
+    });
+
+    describe('with jest-axe', () => {
+
+        it('a11y', async () => {
+            const html = ReactDOMServer.renderToString(<Input
+                type="text"
+                name="test"
+                id="test"
+                value="test"
+                onChange={(c) => c}
+            />);
+
+            const results = await jestAxe(html)
+
+            expect(results).toHaveNoViolations()
+        });
     });
 });
