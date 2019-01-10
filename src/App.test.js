45df29a4292de63ce97961cb049599ad9d319c1a
benoit.deglane <benoit.deglane.externe@capgemini.com>
2019-01-10 17:09:44 +0100

    :white_check_mark: set up axe test

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+import { shallow } from 'enzyme';
+
+test('renders without crashing', () => {
+  shallow(<App />);
 });
---
+++ src/component/input/input.component.test.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import axe from 'axe-core';
+
+import { Input } from './input.component';
+import { mountToDoc } from '../../setupTests';
+
+
+fdescribe('<Input />', () => {
+
+    let input;
+    let component;
+
+    beforeEach(() => {
+
+        input = mountToDoc(<Input
+            type="text"
+            name="test"
+            id="test"
+            value="test"
+            onChange={(c) => c}
+        />);
+
+        component = input.getDOMNode();
+    });
+
+
+    test('a11y', async () => {
+
+        const res = await axe.run(component, {
+            runOnly: {
+                values: ["wcag2a", "wcag2aa"]
+            },
+            resultTypes: ["violations"]
+        });
+
+        if (res.violations.length > 0) {
+            console.log(res.violations);
+        }
+
+        expect(res.violations.length).toBe(0);
+    });
+});
