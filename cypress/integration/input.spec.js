aece4d548fd9ac1fe7a55d677ec36f1d5d42e3bd
mcampourcy <11388201+mcampourcy@users.noreply.github.com>
2019-01-14 14:41:05 +0100

    :white_check_mark: Add plugin cypress axe core (#8)

--- cypress/integration/input.spec.js
+++ cypress/integration/input.spec.js
@@ -12,4 +12,20 @@ describe('Logging In - HTML Web Form', () => {
         .should('contain', 'Entrez une adresse email en @ nantes js .fr')
     })
   })
+
+  context('Axe core test', () => {
+    beforeEach(() => {
+      cy.visit('/')
+      cy.injectAxe()
+    })
+
+    it('Has no detectable a11y violations on load', () => {
+      cy.checkA11y()
+    })
+
+    it('Has no a11y violations after button click', () => {
+      cy.get('button').click()
+      cy.checkA11y()
+    })
+  })
 })
