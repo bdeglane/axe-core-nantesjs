2e73ef10e87d815d8d98d58824214a9c8437385b
mcampourcy <11388201+mcampourcy@users.noreply.github.com>
2019-01-15 22:04:37 +0100

    :white_check_mark: Add cypress test (#11)

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,30 @@ import './App.css'
 import { Form } from './component/Form'
 
 class App extends Component {
+  state = {
+    titleClass: 'normal',
+  }
+
+  handleClick = () => {
+    const { titleClass } = this.state
+    const newClass = titleClass === 'normal' ? 'notVisible' : 'normal'
+
+    this.setState({ titleClass: newClass })
+  }
+
   render() {
+    const { titleClass } = this.state
+
     return (
       <main className="App">
         <section>
-          <h1 tabIndex="0">
+          <h1 tabIndex="0" className={titleClass}>
             Bienvenue à ce meetup
           </h1>
           <p tabIndex="0">
             C'est un faux formulaire, rien que pour vos yeux.
           </p>
-          <Form />
+          <Form buttonAction={this.handleClick} />
         </section>
       </main>
     );
