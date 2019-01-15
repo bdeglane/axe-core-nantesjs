45c33c21eae145c99ed7f308452a8d0f998422e3
Benoît Deglane <bdeglane@users.noreply.github.com>
2019-01-15 22:05:01 +0100

    :white_check_mark: add test app (#12)

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,8 +1,33 @@
 import React from 'react';
+import ReactDOMServer from 'react-dom/server'
 import App from './App';
-
+import { axe as jestAxe } from 'jest-axe'
 import { shallow } from 'enzyme';
 
-test('renders without crashing', () => {
-  shallow(<App />);
-});
+describe('<App />', () => {
+
+  let jestAxeComponent
+
+  beforeEach(() => {
+    const component = (< App />)
+
+    jestAxeComponent = ReactDOMServer.renderToString(component)
+  })
+
+  it('renders without crashing', () => {
+    shallow(<App />)
+  })
+
+  describe('a11y', () => {
+    it('should be valid', async () => {
+      const results = await jestAxe(jestAxeComponent, {
+        rules: {
+          // for demonstration only, don't disable rules that need fixing.
+          // 'aria-valid-attr-value': { enabled: false }
+        }
+      })
+
+      expect(results).toHaveNoViolations()
+    })
+  })
+})
